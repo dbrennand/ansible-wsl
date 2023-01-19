c679ba51436b77979edf2f992f85edf434bf09e8
dbrennand <52419383+dbrennand@users.noreply.github.com>
2023-01-19 20:41:36 +0000

    feat: add progress

---
+++ main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Install python3-pip and python3.10-venv to get us going
+sudo apt-get update && sudo apt-get install python3-pip python3.10-venv -y
+# Create Python venv
+mkdir -pv ~/.venv && python3 -m venv ~/.venv/ansible-wsl
+# Activate venv
+source ~/.venv/ansible-wsl/bin/activate
+# Install dependencies
+python -m pip install ansible==7.1.0 ansible-lint==6.11.0
+ansible-galaxy install -r requirements.yml
+# Run the Ansible playbook
+ansible-playbook --ask-become-pass ansible-wsl.yml
+# Cleanup
+# Uninstall the initial system packages as we have the equivilents installed via homebrew
+sudo apt-get purge python3-pip python3.10-venv -y
+rm -rf ~/.venv/
